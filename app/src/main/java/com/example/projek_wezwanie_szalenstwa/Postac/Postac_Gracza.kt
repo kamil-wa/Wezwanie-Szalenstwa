0f8467d417b5422cbfb519c6da956efebe146e5b
kamil-wa <kamil.w.ofi@gmail.com>
2023-01-11 19:07:50 +0100

    v 0.0.1 to do statystyki przejścia losowanie i tego zapisywanie

---
+++ app/src/main/java/com/example/projek_wezwanie_szalenstwa/Postac/Postac_Gracza.kt
@@ -0,0 +1,12 @@
+package com.example.projek_wezwanie_szalenstwa.Postac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projek_wezwanie_szalenstwa.R
+
+class Postac_Gracza : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_postac_gracza)
+    }
+}
---
+++ app/src/main/java/com/example/projek_wezwanie_szalenstwa/Postac/Tworzenie_Postaci.kt
@@ -0,0 +1,14 @@
+package com.example.projek_wezwanie_szalenstwa.Postac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projek_wezwanie_szalenstwa.R
+
+class Tworzenie_Postaci : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tworzenie_postaci)
+    }
+
+
+}
