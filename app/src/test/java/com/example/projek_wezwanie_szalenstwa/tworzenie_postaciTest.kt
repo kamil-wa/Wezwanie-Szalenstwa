56c8b6ce89d2d044cf8855067fd992ec20d8076e
kamil-wa <kamil.w.ofi@gmail.com>
2023-01-27 18:16:32 +0100

    v. 0.6.2 przerwa

---
+++ app/src/test/java/com/example/projek_wezwanie_szalenstwa/tworzenie_postaciTest.kt
@@ -0,0 +1,49 @@
+package com.example.projek_wezwanie_szalenstwa
+
+import android.widget.Button
+import android.widget.TextView
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+import org.junit.Assert.assertTrue
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest=Config.NONE)
+class TworzeniePostaciTest {
+    private lateinit var activity: tworzenie_postaci
+
+    @Before
+    fun setUp() {
+        activity = Robolectric.buildActivity(tworzenie_postaci::class.java).create().get()
+    }
+
+    @Test
+    fun testRollStrengthValue() {
+        val buttonRollStrength = activity.findViewById<Button>(R.id.rollStrength)
+        buttonRollStrength.performClick()
+
+        val strengthTextView = activity.findViewById<TextView>(R.id.strengthValue)
+        val strengthValue = strengthTextView.text.toString().toInt()
+        assertTrue(strengthValue in 10..50)
+    }
+
+    @Test
+    fun testRollDexterityValue() {
+        val buttonRollDexterity = activity.findViewById<Button>(R.id.rollDexterity)
+        buttonRollDexterity.performClick()
+
+        val dexterityTextView = activity.findViewById<TextView>(R.id.dexterityValue)
+        val dexterityValue = dexterityTextView.text.toString().toInt()
+        assertTrue(dexterityValue in 10..40)
+    }
+
+}
